f91c121181aff8731148959b8b90fe570c5be99c
Marcus Little <mcolelittle@gmail.com>
2024-07-08 17:04:34 -0400

    add test path

--- netlify/edge-functions/hello.ts
+++ netlify/edge-functions/hello.ts
@@ -3,3 +3,6 @@ import type { Context } from "@netlify/edge-functions";
 export default async (request: Request, context: Context) => {
   return new URL("/https://www.google.com/", request.url);
 };
+
+export const config = { path: "/test" };
+
